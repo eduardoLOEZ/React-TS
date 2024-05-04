b2ffdca6bb6055d192e6dca1943f4ba2fbd17483
Eduardo Loeza <eduardoloeza@Eduardos-MacBook-Air.local>
2024-05-03 22:30:48 -0600

    first commit

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todos";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "do math tasks 🧮",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "do english tasks 🇺🇸",
+    completed: false,
+  },
+  {
+    id: 1,
+    title: "do coding tasks 👨🏻‍💻",
+    completed: true,
+  },
+];
